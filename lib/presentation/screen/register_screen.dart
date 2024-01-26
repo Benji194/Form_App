7b4674cddaf9935a4248dacaae11cb6401f0f8b1
Arturo Benjamin Vazquez Muñoz <vazben94@gmail.com>
2024-01-26 11:53:18 -0600

    Formularios con gestor de estado

--- lib/presentation/screen/register_screen.dart
+++ lib/presentation/screen/register_screen.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:form_app/presentation/blocs/register_cubit/register_cubit.dart';
 import 'package:form_app/presentation/widgets/widgets.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
 
-
-
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Nuevo Usuario'),
       ),
-      body: const _RegisterView(),
+      body:  BlocProvider(
+        create: (context) => RegisterCubit(),
+        child: const _RegisterView(),
+      ),
     );
   }
 }
-
-
 
 class _RegisterView extends StatelessWidget {
   const _RegisterView();
 
   @override
   Widget build(BuildContext context) {
-    return  const SafeArea(
+    return const SafeArea(
       child: Padding(
-        padding:  EdgeInsets.symmetric( horizontal: 10 ),
+        padding: EdgeInsets.symmetric(horizontal: 10),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
-            children:  [
-               FlutterLogo( size: 100, ),
-               _RegisterForm(),
-               SizedBox(height: 20,),
+            children: [
+              FlutterLogo(
+                size: 100,
+              ),
+              _RegisterForm(),
+              SizedBox(
+                height: 20,
+              ),
             ],
           ),
         ),
@@ -50,64 +55,76 @@ class _RegisterForm extends StatefulWidget {
 }
 
 class _RegisterFormState extends State<_RegisterForm> {
-
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-
-  String username = '';
-  String email = '';
-  String password = '';
 
   @override
   Widget build(BuildContext context) {
-    return  Form(
+    final registerCubit = context.watch<RegisterCubit>();
+
+    return Form(
       key: _formkey,
       child: Column(
         children: [
           CustomTextField(
             label: 'Nombre de Usuario',
-            onChanged: (value)=> username  = value,  
+            onChanged: (value) {
+              registerCubit.usernameChanged(value);
+              _formkey.currentState?.validate();
+            },
             validator: (value) {
-              if (value == null || value.isEmpty ) return 'Campo Requerido';
-              if (value.trim().isEmpty ) return 'Campo Requerido';
-              if (value.trim().length <6 ) return 'Se requiere mas de 5 letras';
+              if (value == null || value.isEmpty) return 'Campo Requerido';
+              if (value.trim().isEmpty) return 'Campo Requerido';
+              if (value.trim().length < 6) return 'Se requiere mas de 6 letras';
               return null;
             },
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           CustomTextField(
             label: 'Correo Electronico',
-            onChanged: (value)=> email = value ,  
+            onChanged: (value) {
+              registerCubit.emailChanged(value);
+              _formkey.currentState?.validate();
+            },
             validator: (value) {
-              if (value == null || value.isEmpty ) return 'Campo Requerido';
-              if (value.trim().isEmpty ) return 'Campo Requerido';
+              if (value == null || value.isEmpty) return 'Campo Requerido';
+              if (value.trim().isEmpty) return 'Campo Requerido';
               final emailRegExp = RegExp(
-                  r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
-                );
-              if ( !emailRegExp.hasMatch(value) ) return 'No tiene formato de correo';
+                r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+              );
+              if (!emailRegExp.hasMatch(value))
+                return 'No tiene formato de correo';
               return null;
             },
-            
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           CustomTextField(
             label: 'Contraseña',
             obscureText: true,
-            onChanged: (value)=> password = value ,  
+            onChanged: (value) {
+              registerCubit.passwordChanged(value);
+              _formkey.currentState?.validate();
+            },
             validator: (value) {
-              if (value == null || value.isEmpty ) return 'Campo Requerido';
-              if (value.trim().isEmpty ) return 'Campo Requerido';
-              if (value.trim().length <6 ) return 'Se requiere mas de 5 letras';
+              if (value == null || value.isEmpty) return 'Campo Requerido';
+              if (value.trim().isEmpty) return 'Campo Requerido';
+              if (value.trim().length < 6) return 'Se requiere mas de 6 letras';
               return null;
             },
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           FilledButton.tonalIcon(
             onPressed: () {
               final isValid = _formkey.currentState!.validate();
-              if (!isValid) return ;
-              print('$username , $email , $password');
+              if (!isValid) return;
+              registerCubit.onSubmit();
             },
-            icon: const Icon(Icons.save),  
+            icon: const Icon(Icons.save),
             label: const Text('Crear Usuario'),
           ),
         ],
